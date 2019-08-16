cee2981605ccfd66f7024cc684f6333c66536856
Cook1619 <cook6348@gmail.com>
2019-08-16 17:19:09 -0500

    Passing props to dummy components

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,6 +1,24 @@
 import React from "react";
 
-class Header extends React.Component {
+
+//Stateless component, you can destructure the props in the function arguements like this instead of just passing in props
+const Header = ({ tagline, age }) => (
+    <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
+//This is a component which you would use to manage state
+/* class Header extends React.Component {
   render() {
     return (
       <header className="top">
@@ -13,10 +31,10 @@ class Header extends React.Component {
           Day
         </h1>
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
   }
-}
+} */
 export default Header;
