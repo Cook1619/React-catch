c73f9cb85b362e07ffc5e29f0de9c6bad31db933
Cook1619 <cook6348@gmail.com>
2019-08-15 22:30:43 -0500

    Components added for project

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom'
-import StorePicker from '../src/components/StorePicker'
+import App from '../src/components/App'
+import './css/style.css';
 
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
 
