6e9b71a3b2427b01d9a7162c0c163d8f4300b6e6
Cook1619 <cook6348@gmail.com>
2019-08-22 18:41:27 -0500

    Taking out creds

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAQ2xTuQSeNK0Jgq9grnT-Ai0vS-jfDGt4",
+  apiKey: "",
   authDomain: "catch-of-the-day-matt-cook.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-matt-cook.firebaseio.com"
 });
