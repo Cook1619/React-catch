99af9789360d5b925dd03f735447f5c465a8de8f
Cook1619 <cook6348@gmail.com>
2019-08-22 19:24:59 -0500

    Finihsing order component to persist through local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
-import Fish from "./Fish";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 import base from "../base";
 
 class App extends React.Component {
@@ -14,14 +14,28 @@ class App extends React.Component {
 
   componentDidMount() {
     const { params } = this.props.match;
+    // first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+
     this.ref = base.syncState(`${params.storeId}/fishes`, {
-        context: this,
-        state: "fishes"
+      context: this,
+      state: "fishes"
     });
   }
 
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
   componentWillUnmount() {
-     base.removeBinding(this.ref) 
+    base.removeBinding(this.ref);
   }
 
   addFish = fish => {
@@ -32,15 +46,20 @@ class App extends React.Component {
     // 3. Set the new fishes object to state
     this.setState({ fishes });
   };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
 
   addToOrder = key => {
+    // 1. take a copy of state
     const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
     this.setState({ order });
   };
+
   render() {
     return (
       <div className="catch-of-the-day">
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,9 +5,15 @@ class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    if (!fish.status === "available") {
+    const isAvailable = fish && fish.status === "available";
+    // Make sure the fish is loaded before we continue!
+    if (!fish) return null;
+
+    if (!isAvailable) {
       return (
-        <li key={key}>Sorry the {fish ? fish.name : "fish"} is no longer available</li>
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
       );
     }
     return (
@@ -40,4 +46,5 @@ class Order extends React.Component {
     );
   }
 }
+
 export default Order;
