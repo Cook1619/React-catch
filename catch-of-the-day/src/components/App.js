92875a52b4660f9a19fb4e12f970692276be91f8
Cook1619 <cook6348@gmail.com>
2019-11-19 18:59:42 -0600

    Adding ability to remove fish and animate adding fish and

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -56,6 +56,12 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = (key) => {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null
+    this.setState({fishes});
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -68,6 +74,14 @@ class App extends React.Component {
     // 3. Call setState to update our state object
     this.setState({ order });
   };
+
+  removeFromOrder = key => {
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
+    delete order[key];
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  }
 
   render() {
     return (
@@ -85,10 +99,11 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder}/>
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
